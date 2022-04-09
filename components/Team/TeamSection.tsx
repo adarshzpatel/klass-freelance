4f7a95133b9929d5bbb866a9b428215fc6b26847
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-04-09 10:06:20 +0530

    updated profiile pics

--- components/Team/TeamSection.tsx
+++ components/Team/TeamSection.tsx
@@ -19,12 +19,12 @@ const TeamSection = () => {
       <Heading text="Meet the team" />
       <div className="flex gap-4 mt-16 flex-wrap justify-center">
       <ProfileCard name="Kaycee" pfp="/1.jpg" role="Founder" />
-      <ProfileCard name="Maxi" pfp="/23.jpg" role="Co-founder" />
-      <ProfileCard name="Remi" pfp="/26.jpg" role="Co-Founder / Artist" />
-      <ProfileCard name="Adarsh" pfp="/33.jpg" role="Co-Founder / Engineer" />
-      <ProfileCard name="Chinu" pfp="/1.jpg" role="Project Manager" />
-      <ProfileCard name="Ranza" pfp="/26.jpg" role="Head Mod" />
-      <ProfileCard name="Vivian" pfp="/33.jpg" role="Collab Manager" />
+      <ProfileCard name="Maxi" pfp="/2.jpg" role="Co-founder" />
+      <ProfileCard name="Remi" pfp="/3.jpg" role="Co-Founder / Artist" />
+      <ProfileCard name="Adarsh" pfp="/4.jpg" role="Co-Founder / Engineer" />
+      <ProfileCard name="Chinu" pfp="/5.jpg" role="Project Manager" />
+      <ProfileCard name="Ranza" pfp="/6.jpg" role="Head Mod" />
+      <ProfileCard name="Vivian" pfp="/7.jpg" role="Collab Manager" />
       </div>
       </Container>
     </section>
